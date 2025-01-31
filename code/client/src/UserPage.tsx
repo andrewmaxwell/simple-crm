e4db208b35abf33fd38619aaf86a11cccf68ab6e
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2025-01-31 12:32:18 -0600

    added React router to UI

---
+++ code/client/src/UserPage.tsx
@@ -0,0 +1,1 @@
+export default () => <h1>User Page</h1>
--- code/client/src/main.tsx
+++ code/client/src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
+import UserPage from "./UserPage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<App />} />
+                <Route path="users/:id" element={<UserPage />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
 );
