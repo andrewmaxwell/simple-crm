b3f381496d448aa4c53eaa547d876b7e1c17bce1
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2025-01-31 16:43:38 -0600

    clean up

--- code/client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { Users } from "./users";
-
-const App = () => <Users />;
-
-export default App;
--- code/client/src/main.tsx
+++ code/client/src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import UserPage from "./UserPage.tsx";
 import Layout from "./Layout.tsx";
+import { Users } from "./users.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route index element={<App />} />
+                    <Route index element={<Users />} />
                     <Route path="users/:id" element={<UserPage />} />
                 </Route>
             </Routes>
